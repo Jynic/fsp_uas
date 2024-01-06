657b0c64dae6a039fb7022c4c758259fa50ac9ec
Styaaa <s160421048@student.ubaya.ac.id>
2024-01-06 21:53:30 +0800

    update home

--- home.php
+++ home.php
@@ -14,79 +14,87 @@ $con = new mysqli("localhost", "root", "", "fsp_uas");
 <body>
     <h1>CERBUNG</h1>
     <h3>Cerita Bersambung</h3>
-    <div id='Container-Home'>
+    <div id='container-home'>
         <div class='card_generalkiri'>
-            <h2>Ceritaku</h2><br>
-            <?php 
-            $idusers = "160421054";
-            $limit = "%";
-            if(isset($_GET['limit'])){
-                $limit = $_GET['limit'];
-            }else{
-                $limit = "2";
-            }
-            ?>
-            <script>
-                var id = "<?php echo $idusers; ?>";
-                var limit = "<?php echo $limit; ?>";
-                $.post('ajax_paging_kiri.php', {iduser:id, limit:limit}).done(function
-                (data){
-                    var ceritas = JSON.parse(data);
-                    $.each(ceritas, function(i, item){
-                        $(".card_generalkiri").append("<div id='"+item.idcerita+"' class='card_cerbung'><h2 class='judulCeritaku'>"+item.judul+"</h2><p class='isicard'>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
+            <div class="ceritaku">
+                <h2>Ceritaku</h2><br>
+            </div>
+            <div class="cerita_saya">
+                <?php 
+                $idusers = "160421054";
+                $limit = "%";
+                if(isset($_GET['limit'])){
+                    $limit = $_GET['limit'];
+                }else{
+                    $limit = "2";
+                }
+                ?>
+                <script>
+                    var id = "<?php echo $idusers; ?>";
+                    var limit = "<?php echo $limit; ?>";
+                    $.post('ajax_paging_kiri.php', {iduser:id, limit:limit}).done(function
+                    (data){
+                        var ceritas = JSON.parse(data);
+                        $.each(ceritas, function(i, item){
+                            $(".cerita_saya").append("<div id='"+item.idcerita+"' class='card_cerbung'><h2 class='judulCeritaku'>"+item.judul+"</h2><p class='isicard'>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
+                        });
+                        $(".card_generalkiri").append("<button id='btnTampilkanCeritaku'>Tampilkan Cerita Selanjutnya</button>");
+                        $("#btnTampilkanCeritaku").click(function(){
+                    window.location.href = "home.php?limit=100";
                     });
-                    $(".card_generalkiri").append("<button id='btnTampilkanCeritaku'>Tampilkan Cerita Selanjutnya</button>");
-                    $("#btnTampilkanCeritaku").click(function(){
-                window.location.href = "home.php?limit=100";
-                });
-            
-            
-                });
-            </script>
+                
+                
+                    });
+                </script>
+            </div>
         
         </div>
         <div id='garis_vertikal'></div>
         <div class='card_generalkanan'>
-            <h2>Kumpulan Cerita</h2><br>
-            <?php 
-            $stmt = $con->prepare("select c.idcerita, c.judul, c.idusers_pembuat_awal, count(p.idparagraf) as jumlah_paragraf 
-            from cerita c right join paragraf p on c.idcerita = p.idcerita where c.idusers_pembuat_awal not like ? group by c.idcerita limit ? ;");   
-            $idusers = "160421054";
-            $limit = "%";
-            if(isset($_GET['limitcer'])){
-                $limit = $_GET['limitcer'];
-            }else{
-                $limit = "8";
-            }
-            // $stmt->bind_param("si", $idusers, $limit);
-            // $stmt->execute();
-            // $res = $stmt->get_result();
-            // while($row = $res->fetch_assoc()){
-            //     echo "<div id='".$row['idcerita']."' class='card_cerbung'>
-            //     <h2 class='judulCeritaku'>".$row['judul']."</h2>
-            //     <p class='isicard'>Jumlah Paragraf : ".$row['jumlah_paragraf']."</p>
-            //     <a href class='isicard'>Baca Lebih Lanjut</a>
-            //     </div>";
-            // }
-            ?>
-            <script>
-            var id = "<?php echo $idusers; ?>";
-            var limit = "<?php echo $limit; ?>";
-            $.post('ajax_paging_kanan.php', {iduser:id, limit:limit}).done(function
-            (data){
-                var ceritas = JSON.parse(data);
-                $.each(ceritas, function(i, item){
-                    $(".card_generalkanan").append("<div id='"+item.idcerita+"' class='card_cerbung2'><h2 class='judulCeritaku'>"+item.judul+"</h2><p class='isicard'>Pemilik : "+item.nama+"</p><br><p class='isicard'>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
+            <div class="ceritaku">
+                <h2 >Kumpulan Cerita</h2>
+            </div>
+            <div class="kumpulan_cerita">
+                <?php 
+                $stmt = $con->prepare("select c.idcerita, c.judul, c.idusers_pembuat_awal, count(p.idparagraf) as jumlah_paragraf 
+                from cerita c right join paragraf p on c.idcerita = p.idcerita where c.idusers_pembuat_awal not like ? group by c.idcerita limit ? ;");   
+                $idusers = "160421054";
+                $limit = "%";
+                if(isset($_GET['limitcer'])){
+                    $limit = $_GET['limitcer'];
+                }else{
+                    $limit = "8";
+                }
+                // $stmt->bind_param("si", $idusers, $limit);
+                // $stmt->execute();
+                // $res = $stmt->get_result();
+                // while($row = $res->fetch_assoc()){
+                //     echo "<div id='".$row['idcerita']."' class='card_cerbung'>
+                //     <h2 class='judulCeritaku'>".$row['judul']."</h2>
+                //     <p class='isicard'>Jumlah Paragraf : ".$row['jumlah_paragraf']."</p>
+                //     <a href class='isicard'>Baca Lebih Lanjut</a>
+                //     </div>";
+                // }
+                ?>
+                <script>
+                var id = "<?php echo $idusers; ?>";
+                var limit = "<?php echo $limit; ?>";
+                $.post('ajax_paging_kanan.php', {iduser:id, limit:limit}).done(function
+                (data){
+                    var ceritas = JSON.parse(data);
+                    $.each(ceritas, function(i, item){
+                        $(".kumpulan_cerita").append("<div id='"+item.idcerita+"' class='card_cerbung2'><h2 class='judulCeritaku'>"+item.judul+"</h2><p class='isicard'>Pemilik : "+item.nama+"</p><br><p class='isicard'>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
+                    });
+                    $(".card_generalkanan").append("<button id='btnTampilKumpulanCerita'>Tampilkan Cerita Selanjutnya</button>");
+                    $("#btnTampilKumpulanCerita").click(function(){
+                window.location.href = "home.php?limitcer=100";
                 });
-                $(".card_generalkanan").append("<button id='btnTampilKumpulanCerita'>Tampilkan Cerita Selanjutnya</button>");
-                $("#btnTampilKumpulanCerita").click(function(){
-            window.location.href = "home.php?limitcer=100";
-            });
+                
+                
+                });
             
-            
-            });
-        
-            </script>
+                </script>
+            </div>
             
         </div>
     </div>
@@ -94,17 +102,15 @@ $con = new mysqli("localhost", "root", "", "fsp_uas");
         <br>
         <p>Ketegori : </p>
         <select name="cboKategori" id="cbokategori"> 
-            <option id='optKumpulancerita slc' value="kumpulancerita">Kumpulan Cerita</option>
+            <option id='optKumpulancerita' value="kumpulancerita">Kumpulan Cerita</option>
             <option id='optCeritaku' value="ceritaku">Ceritaku</option>
         </select>
         <div id='Container-Kecil'>
             <h1>Kumpulan Cerita</h1>
             <div id='card_cerita'>
-                <div class='card_cerbung_kecil'>
-                    <h2 class='judulCeritaku'>CERIAKU</h2>
-                    <p class='item_kecil'>penulis : Dummy</p>
-                    <p class='item_kecil'>jumlah par : 2</p>
-                </div>
+                <!-- <div class='card_cerbung_kecil'>
+                    
+                </div> -->
                 <?php 
                 $idusers = "160421054";
                 $limit = "%";
@@ -116,22 +122,65 @@ $con = new mysqli("localhost", "root", "", "fsp_uas");
                 
                 ?>
                 <script>
+                    $.post('ajax_paging_kanan.php', {iduser:id, limit:4}).done(function
+                        (data){
+                            var ceritas = JSON.parse(data);
+                            $.each(ceritas, function(i, item){
+                                $("#card_cerita").append("<div id='"+item.idcerita+"' class='card_cerbung_kecil'><h2 class='judulCeritaku'>"+item.judul+"</h2><p class=''>Pemilik : "+item.nama+"</p><br><p class=''>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
+                            });
+                            $(".card_cerita").append("<button id='btnTampilKumpulanCerita'>Tampilkan Cerita Selanjutnya</button>");
+                            $("#btnTampilKumpulanCerita").click(function(){
+                        window.location.href = "home.php?limitkecil=100";
+                        });
+                        
+                        
+                        });
+                
+                $("#cbokategori").change(function() {
+                    var kategori = $(this).val();
+                    var id = "<?php echo $idusers; ?>";
+                    var limit = "<?php echo $limit; ?>";
+
                     
-                var id = "<?php echo $idusers; ?>";
-                var limit = "<?php echo $limit; ?>";
-                $.post('ajax_paging_kanan.php', {iduser:id, limit:limit}).done(function
-                (data){
-                    var ceritas = JSON.parse(data);
-                    $.each(ceritas, function(i, item){
-                        $("#card_cerita").append("<div id='"+item.idcerita+"' class='card_cerbung_kecil'><h2 class='judulCeritaku'>"+item.judul+"</h2><p class=''>Pemilik : "+item.nama+"</p><br><p class=''>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
-                    });
-                    $(".card_cerita").append("<button id='btnTampilKumpulanCerita'>Tampilkan Cerita Selanjutnya</button>");
-                    $("#btnTampilKumpulanCerita").click(function(){
-                window.location.href = "home.php?limitkecil=100";
-                });
+
+                    
+
+                    if(kategori == "kumpulancerita"){
+                        document.getElementById("card_cerita").innerHTML = "";
+
+                        $.post('ajax_paging_kanan.php', {iduser:id, limit:limit}).done(function
+                        (data){
+                            var ceritas = JSON.parse(data);
+                            $.each(ceritas, function(i, item){
+                                $("#card_cerita").append("<div id='"+item.idcerita+"' class='card_cerbung_kecil'><h2 class='judulCeritaku'>"+item.judul+"</h2><p class=''>Pemilik : "+item.nama+"</p><br><p class=''>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
+                            });
+                            $(".card_cerita").append("<button id='btnTampilKumpulanCerita'>Tampilkan Cerita Selanjutnya</button>");
+                            $("#btnTampilKumpulanCerita").click(function(){
+                        window.location.href = "home.php?limitkecil=100";
+                        });
+                        
+                        
+                        });
+                    } else {
+                        document.getElementById("card_cerita").innerHTML = "";
+
+                        $.post('ajax_paging_kiri.php', {iduser:id, limit:limit}).done(function
+                        (data){
+                            var ceritas = JSON.parse(data);
+                            $.each(ceritas, function(i, item){
+                                $("#card_cerita").append("<div id='"+item.idcerita+"' class='card_cerbung_kecil'><h2 class='judulCeritaku'>"+item.judul+"</h2><br><p class=''>Jumlah Paragraf : "+item.jumlah_paragraf+"</p><a href class='isicard'>Baca Lebih Lanjut</a></div>");
+                            });
+                            $(".card_cerita").append("<button id='btnTampilKumpulanCerita'>Tampilkan Cerita Selanjutnya</button>");
+                            $("#btnTampilKumpulanCerita").click(function(){
+                        window.location.href = "home.php?limitkecil=100";
+                        });
+                        
+                        
+                        });
+                    }
+                })
+
                 
-                
-                });
             
                 </script>
             </div>
